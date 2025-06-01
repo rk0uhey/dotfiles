1b55c548b97e64cc064153b9d83c33e1ff05f94d
rk0uhey <rk0uhey@gmail.com>
2025-06-01 18:29:32 +0900

    chore: Add macOS defaults script for Dock and Finder configuration

---
+++ dotfiles/macos/defaults.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -eu
+
+app_list=(
+	"Dock"
+	"Finder"
+)
+
+# Dock
+## Dock の位置（左）
+defaults write com.apple.dock orientation -string "left"
+## Dock のサイズ
+defaults write com.apple.dock tilesize -int 60
+## 拡大機能を無効化
+defaults write com.apple.dock magnification -bool false
+## 起動中のアプリにインジケータを表示
+defaults write com.apple.dock show-process-indicators -bool true
+## 最近使ったアプリを非表示
+defaults write com.apple.dock show-recents -bool false
+## Dock の自動非表示を無効化
+defaults write com.apple.dock autohide -bool false
+## アプリ最小化をアプリに格納しない
+defaults write com.apple.dock minimize-to-application -bool false
+
+# Finder
+## 隠しファイルを表示
+defaults write com.apple.finder AppleShowAllFiles -bool true
+
+# 再起動
+for app in "${app_list[@]}"; do
+	killall "${app}" &> /dev/null
+done
